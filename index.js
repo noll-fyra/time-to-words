65a299443dedcc73d39e0bdce70d247da6b8823a
Jonathan Ng <jonathanlouisng@gmail.com>
2025-01-22 10:40:08 +0800

    add initial version of time function

--- index.js
+++ index.js
@@ -1,11 +1,136 @@
 // expecting time to be a string in the format like '8:15' or '12:30'
 function convertTimeToWords(time) {
-  // TODO: real code goes here!
-  if (time === '0:00') {
-    return 'midnight';
+  // split time into hour and minutes > [h,m]
+  function splitTime(time) {
+    return time.split(":");
   }
 
-  return 'half past eight';
+  // convert hour from number to text
+  function convertTimeIntoText(time) {
+    switch (parseInt(time)) {
+      case 1:
+        return "one";
+      case 2:
+        return "two";
+      case 3:
+        return "three";
+      case 4:
+        return "four";
+      case 5:
+        return "five";
+      case 6:
+        return "six";
+      case 7:
+        return "seven";
+      case 8:
+        return "eight";
+      case 9:
+        return "nine";
+      case 10:
+        return "ten";
+      case 11:
+        return "eleven";
+      case 12:
+        return "twelve";
+      case 13:
+        return "thirteen";
+      case 14:
+        return "fourteen";
+      case 15:
+        return "fifteen";
+      case 16:
+        return "sixteen";
+      case 17:
+        return "seventeen";
+      case 18:
+        return "eighteen";
+      case 19:
+        return "nineteen";
+      case 20:
+        return "twenty";
+      case 21:
+        return "twenty one";
+      case 22:
+        return "twenty two";
+      case 23:
+        return "twenty three";
+      case 24:
+        return "twenty four";
+      case 25:
+        return "twenty five";
+      case 26:
+        return "twenty six";
+      case 27:
+        return "twenty seven";
+      case 28:
+        return "twenty eight";
+      case 29:
+        return "twenty nine";
+      // case 30:
+      //   return "thirty";
+      // case 40:
+      //   return "forty";
+      // case 50:
+      //   return "fifty";
+      default:
+        return "";
+    }
+  }
+
+  const [hour, minutes] = splitTime(time);
+
+  // TODO: real code goes here!
+  if (time === "0:00") {
+    return "midnight";
+  }
+
+  if (time === "12:00") {
+    return "midday";
+  }
+
+  if (minutes === "00") {
+    return convertTimeIntoText(hour) + " o'clock";
+  }
+
+  if (minutes === "15") {
+    return "quarter past " + convertTimeIntoText(hour);
+  }
+
+  if (minutes === "30") {
+    return "half past " + convertTimeIntoText(hour);
+  }
+
+  if (minutes === "45") {
+    return "quarter to " + convertTimeIntoText((parseInt(hour) % 12) + 1);
+  }
+
+  if (parseInt(minutes) >= 0 && parseInt(minutes) <= 29) {
+    return convertTimeIntoText(minutes) + " past " + convertTimeIntoText(hour);
+  }
+
+  if (parseInt(minutes) >= 31 && parseInt(minutes) <= 59) {
+    return (
+      convertTimeIntoText(60 - parseInt(minutes)) +
+      " to " +
+      convertTimeIntoText((parseInt(hour) % 12) + 1)
+    );
+  }
+
+  // if (time) return "half past eight";
 }
+
+convertTimeToWords("8:14");
+
+// '0:00' > 'midnight'
+// '12:00' > 'midday'
+// '2:00' > 'two o'clock'
+// '2:03' > 'three past two'
+// '2:11' > 'eleven past two'
+// '2:15' > 'quarter past two'
+// '2:30' > 'half past two'
+// '2:33' > 'twenty seven to three'
+// '2:40' > 'twenty to three'
+// '2:45' > 'quarter to three'
+// '2:55' > 'five to three'
 
 module.exports = { convertTimeToWords };
--- index.test.js
+++ index.test.js
@@ -1,18 +1,70 @@
-const  { convertTimeToWords } = require('./index');
+const { convertTimeToWords } = require("./index");
 
-describe('Time to words', () => {
-  it('Handles midnight', () => {
-    const timeInWords = convertTimeToWords('0:00');
-    expect(timeInWords).toBe('midnight');
+describe("Time to words", () => {
+  it("Handles midnight", () => {
+    const timeInWords = convertTimeToWords("0:00");
+    expect(timeInWords).toBe("midnight");
   });
 
-  it('Handles 30 - 8:30', () => {
-    const timeInWords = convertTimeToWords('8:30');
-    expect(timeInWords).toBe('half past eight');
+  it("Handles midday", () => {
+    const timeInWords = convertTimeToWords("12:00");
+    expect(timeInWords).toBe("midday");
   });
 
-  it('Handles times after 30 mins - 2:45', () => {
-    const timeInWords = convertTimeToWords('2:45');
-    expect(timeInWords).toBe('quarter to three');
+  it("Handles on the hour - 8:00", () => {
+    const timeInWords = convertTimeToWords("8:00");
+    expect(timeInWords).toBe("eight o'clock");
+  });
+
+  it("Handles times before 30 - 8:14", () => {
+    const timeInWords = convertTimeToWords("8:14");
+    expect(timeInWords).toBe("fourteen past eight");
+  });
+
+  it("Handles 15 minutes after - 8:15", () => {
+    const timeInWords = convertTimeToWords("8:15");
+    expect(timeInWords).toBe("quarter past eight");
+  });
+
+  it("Handles 30 - 8:30", () => {
+    const timeInWords = convertTimeToWords("8:30");
+    expect(timeInWords).toBe("half past eight");
+  });
+
+  it("Handles times after 30 mins - 2:44", () => {
+    const timeInWords = convertTimeToWords("2:44");
+    expect(timeInWords).toBe("sixteen to three");
+  });
+
+  it("Handles times after 30 mins - 2:45", () => {
+    const timeInWords = convertTimeToWords("2:45");
+    expect(timeInWords).toBe("quarter to three");
+  });
+
+  it("Handles times at 40 mins - 2:40", () => {
+    const timeInWords = convertTimeToWords("2:40");
+    expect(timeInWords).toBe("twenty to three");
+  });
+
+  it("Handles times at 43 mins - 2:43", () => {
+    const timeInWords = convertTimeToWords("2:43");
+    expect(timeInWords).toBe("seventeen to three");
+  });
+
+  it("Handles times past 12 - 12:39", () => {
+    const timeInWords = convertTimeToWords("12:39");
+    expect(timeInWords).toBe("twenty one to one");
   });
 });
+
+// '0:00' > 'midnight'
+// '12:00' > 'midday'
+// '2:00' > 'two o'clock'
+// '2:03' > 'three past two'
+// '2:11' > 'eleven past two'
+// '2:15' > 'quarter past two'
+// '2:30' > 'half past two'
+// '2:33' > 'twenty seven to three'
+// '2:40' > 'twenty to three'
+// '2:45' > 'quarter to three'
+// '2:55' > 'five to three'
